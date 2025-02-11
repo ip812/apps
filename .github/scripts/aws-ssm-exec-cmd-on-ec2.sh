53fc4d74f05ff8bc61446c9fb63d20b2c2318572
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-02-11 15:01:41 +0200

    exec cmd

--- .github/scripts/aws-ssm-exec-cmd-on-ec2.sh
+++ .github/scripts/aws-ssm-exec-cmd-on-ec2.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-echo "${AWS_REGION}" 
-echo "${CMD}" 
-echo "${INSTANCE_ID}" 
-
-cmd_id="$(aws ssm send-command \
+CMD_ID="$(aws ssm send-command \
     --region "${AWS_REGION}" \
     --document-name "AWS-RunShellScript" \
     --targets "Key=instanceIds,Values=${INSTANCE_ID}" \
@@ -12,19 +8,19 @@ cmd_id="$(aws ssm send-command \
     --query 'Command.CommandId' \
     --output text)"
 
-cnt=0
+CNT=0
 while true; do
-    cnt=$((cnt + 1))
+    CNT=$((CNT + 1))
     status="$(aws ssm get-command-invocation \
         --region "${AWS_REGION}" \
         --instance-id "${INSTANCE_ID}" \
-        --command-id "${cmd_id}" \
+        --command-id "${CMD_ID}" \
         --query 'Status' \
         --output text)"
     if [[ "${status}" != "InProgress" ]]; then
         break
     fi
-    if [ "${cnt}" -eq 10 ]; then
+    if [ "${CNT}" -eq 10 ]; then
         echo "Command \"${CMD}\"  never ends"
         exit 42
     fi
@@ -34,7 +30,7 @@ done
 aws ssm get-command-invocation \
     --region "${AWS_REGION}" \
     --instance-id "${INSTANCE_ID}" \
-    --command-id "${cmd_id}" \
+    --command-id "${CMD_ID}" \
     --query 'StandardOutputContent' \
     --output text
 
