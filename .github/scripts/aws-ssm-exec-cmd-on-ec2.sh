418d908a0b6e637bd90b7e70f7205cd6a23c8de6
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-02-12 15:36:18 +0200

    adjksa

--- .github/scripts/aws-ssm-exec-cmd-on-ec2.sh
+++ .github/scripts/aws-ssm-exec-cmd-on-ec2.sh
@@ -3,7 +3,14 @@
 set -e
 
 CMD=$1
-INSTACE_ID=$2
+INSTANCE_ID=$2  
+
+if [[ -z "${INSTANCE_ID}" ]]; then
+    echo "Error: INSTANCE_ID is empty. Exiting."
+    exit 1
+fi
+
+echo "Executing command on instance: ${INSTANCE_ID}"
 
 CMD_ID="$(aws ssm send-command \
     --region "${AWS_REGION}" \
@@ -12,25 +19,35 @@ CMD_ID="$(aws ssm send-command \
     --parameters "commands=[\"${CMD}\"]" \
     --query 'Command.CommandId' \
     --output text)"
-echo "AWS command id: ${CMD_ID}"
+
+if [[ -z "${CMD_ID}" ]]; then
+    echo "Error: Failed to send command."
+    exit 1
+fi
+
+echo "AWS command ID: ${CMD_ID}"
 
 CNT=0
 while true; do
     CNT=$((CNT + 1))
-    echo "Try ${CNT} to get the command status"
+    echo "Attempt ${CNT} to get the command status"
+    
     status="$(aws ssm get-command-invocation \
         --region "${AWS_REGION}" \
         --instance-id "${INSTANCE_ID}" \
         --command-id "${CMD_ID}" \
         --query 'Status' \
         --output text)"
+    
     if [[ "${status}" != "InProgress" ]]; then
         break
     fi
-    if [ "${CNT}" -eq 10 ]; then
-        echo "Command \"${CMD}\"  never ends"
+
+    if [[ "${CNT}" -eq 10 ]]; then
+        echo "Error: Command '${CMD}' never completed."
         exit 42
     fi
+
     sleep 1
 done
 
